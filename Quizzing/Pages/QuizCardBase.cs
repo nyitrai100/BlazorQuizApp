32966d6b468780fd911aceb4b32876e00d1bebb1
nyitraibence <nyitrai100@gmail.com>
2024-05-11 12:56:02 +0100

    changed the questions

--- Quizzing/Pages/QuizCardBase.cs
+++ Quizzing/Pages/QuizCardBase.cs
@@ -35,76 +35,77 @@ namespace Quizzing.Pages
 {
     Question q1 = new Question
     {
-        QuestionTitle = "What is the capital city of France?",
-        Options = new List<string>() { "Paris", "London", "Rome", "Berlin" },
-        Answer = "Paris"
+        QuestionTitle = "Who is the leader of the Minions?",
+        Options = new List<string>() { "Bob", "Kevin", "Stuart", "Dave" },
+        Answer = "Kevin"
     };
 
     Question q2 = new Question
     {
-        QuestionTitle = "Who wrote the play 'Romeo and Juliet'?",
-        Options = new List<string>() { "William Shakespeare", "Jane Austen", "Charles Dickens", "Emily Brontë" },
-        Answer = "William Shakespeare"
+        QuestionTitle = "What is the favorite food of the Minions?",
+        Options = new List<string>() { "Banana", "Pizza", "Ice Cream", "Tacos" },
+        Answer = "Banana"
     };
 
     Question q3 = new Question
     {
-        QuestionTitle = "Which planet is known as the 'Red Planet'?",
-        Options = new List<string>() { "Jupiter", "Mars", "Venus", "Saturn" },
-        Answer = "Mars"
+        QuestionTitle = "Which evil master do the Minions serve?",
+        Options = new List<string>() { "Gru", "Vector", "Scarlet Overkill", "Balthazar Bratt" },
+        Answer = "Gru"
     };
 
     Question q4 = new Question
     {
-        QuestionTitle = "Who painted the famous artwork 'Mona Lisa'?",
-        Options = new List<string>() { "Vincent van Gogh", "Leonardo da Vinci", "Pablo Picasso", "Michelangelo" },
-        Answer = "Leonardo da Vinci"
+        QuestionTitle = "What is the name of Gru's loyal scientist assistant?",
+        Options = new List<string>() { "Dr. Nefario", "Professor Flux", "Dr. Evil", "Dr. Brain" },
+        Answer = "Dr. Nefario"
     };
 
     Question q5 = new Question
     {
-        QuestionTitle = "What is the chemical symbol for water?",
-        Options = new List<string>() { "H2O", "CO2", "NaCl", "O2" },
-        Answer = "H2O"
+        QuestionTitle = "Which language do the Minions speak?",
+        Options = new List<string>() { "Minionese", "Banana-speak", "Gibberish", "Minionish" },
+        Answer = "Minionese"
     };
 
     Question q6 = new Question
     {
-        QuestionTitle = "Who wrote the novel 'Pride and Prejudice'?",
-        Options = new List<string>() { "Jane Austen", "Charlotte Brontë", "Emily Dickinson", "George Eliot" },
-        Answer = "Jane Austen"
+        QuestionTitle = "What is the name of the villain who steals the Moon in 'Despicable Me'?",
+        Options = new List<string>() { "Vector", "El Macho", "Balthazar Bratt", "Scarlet Overkill" },
+        Answer = "Vector"
     };
 
     Question q7 = new Question
     {
-        QuestionTitle = "What is the tallest mountain in the world?",
-        Options = new List<string>() { "Mount Kilimanjaro", "Mount Everest", "Mount Fuji", "Mount McKinley" },
-        Answer = "Mount Everest"
+        QuestionTitle = "Who is the eldest Minion?",
+        Options = new List<string>() { "Bob", "Kevin", "Stuart", "Dave" },
+        Answer = "Stuart"
     };
 
     Question q8 = new Question
     {
-        QuestionTitle = "Which continent is the largest by land area?",
-        Options = new List<string>() { "Asia", "Africa", "North America", "Europe" },
-        Answer = "Asia"
+        QuestionTitle = "What is the name of Gru's pet dog in 'Despicable Me'?",
+        Options = new List<string>() { "Kyle", "Max", "Rex", "Buddy" },
+        Answer = "Kyle"
     };
 
     Question q9 = new Question
     {
-        QuestionTitle = "Who invented the telephone?",
-        Options = new List<string>() { "Thomas Edison", "Alexander Graham Bell", "Nikola Tesla", "Albert Einstein" },
-        Answer = "Alexander Graham Bell"
+        QuestionTitle = "What is the name of the island where the Minions are banished in 'Minions'?",
+        Options = new List<string>() { "Isle of Dogs", "Isle of Evil", "Isle of Misfits", "Isle of Mischief" },
+        Answer = "Isle of Misfits"
     };
 
     Question q10 = new Question
     {
-        QuestionTitle = "What is the chemical symbol for gold?",
-        Options = new List<string>() { "Au", "Ag", "Fe", "Pb" },
-        Answer = "Au"
+        QuestionTitle = "What is the name of the Minion who becomes king in 'Minions: The Rise of Gru'?",
+        Options = new List<string>() { "Bob", "Kevin", "Stuart", "Otto" },
+        Answer = "Bob"
     };
 
     Questions.AddRange(new List<Question> { q1, q2, q3, q4, q5, q6, q7, q8, q9, q10 });
 }
+
 
     }
 }
